7ea457e2bdc891e260e697b2ec1190f2351ad9eb
yenkaykiran <yenkaykiran@gmail.com>
2018-03-06 18:55:29 +0530

    CHange Package for Rest Resource

--- server/src/main/java/yuown/yenkay/familytree/rest/MemberLinkResource.java
+++ server/src/main/java/yuown/yenkay/familytree/rest/MemberLinkResource.java
@@ -1,4 +1,4 @@
-package yuown.yenkay.familytree;
+package yuown.yenkay.familytree.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestParam;
