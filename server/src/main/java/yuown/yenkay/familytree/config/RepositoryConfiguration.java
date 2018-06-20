b740bf18346688e55514f96b4cc7d6237f2c157b
yenkaykiran <yenkaykiran@gmail.com>
2018-06-20 07:52:10 +0530

    Generate ID in the response for Gothram Entity

--- server/src/main/java/yuown/yenkay/familytree/config/RepositoryConfiguration.java
+++ server/src/main/java/yuown/yenkay/familytree/config/RepositoryConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
+import yuown.yenkay.familytree.model.Gothram;
 import yuown.yenkay.familytree.model.Member;
 
 @Configuration
@@ -11,6 +12,6 @@ public class RepositoryConfiguration extends RepositoryRestConfigurerAdapter {
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-		config.exposeIdsFor(Member.class);
+		config.exposeIdsFor(Member.class, Gothram.class);
 	}
 }
